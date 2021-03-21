13b07cf4a0cc8e467c1a6e0bc479969cf10297c7
travis_gandy <travis_gandy@yahoo.com>
2021-03-21 01:16:40 -0400

    Done.

--- main.js
+++ main.js
@@ -3,6 +3,55 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+  const errorModal = document.getElementById('modal')
+  const errorMessage = document.getElementById('modal-message')
+
+ function hideError() {
+   errorModal.classList.add('hidden')
+   errorMessage.textContent = ''
+ }
+
+ function showError(displayError) {
+   errorModal.classList.remove('hidden')
+   errorMessage.textContent = displayError
+ }
+
+ function likeListener() {
+   const hearts = document.getElementsByClassName('like-glyph')
+
+   // Iterate over array to access a listener for each like
+   for (heart of hearts) {
+     clickLike(heart)
+   }
+ }
+
+ function clickLike(like) {
+   like.addEventListener("click", () => {
+     // Server call replacing a fetch
+     mimicServerCall()
+    
+     .then((response) => {
+       updateLikes(like)
+     })
+     
+     .catch((error) => {
+       showError(error);
+      
+       setTimeout(hideError, 5000);
+     })
+   })
+ }
+
+ function updateLikes(specificLike) {
+  specificLike.classList.toggle('activated-heart')
+ }
+
+ function main() {
+   hideError()
+   likeListener()
+ }
+
+ main()
 
 
 
